4456bb9f5f04a0e07233b062432aef9936b80ce6
JoA Azria <joa.azria@gmail.com>
2021-05-04 23:21:42 -0300

    creating drink

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,24 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home, :drinks, :beers]
 
   def home
+    # if params[:query].present?
+    #   @drinks = Drink.search_by_name(params[:query])
+    # else
+      @drinks = Drink.all
+    # end
   end
+
+  def drinks
+    @drinks = Drink.all
+  end
+
+  def beers
+    @drinks = Drink.all
+  end
+
+  def profile
+    @drinks = Drink.search_by_name(params[:query])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'pages#home'
+  get '/drinks', to: 'pages#drinks'
+  get '/beers', to: 'pages#beers'
+
+   resources :drinks
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
